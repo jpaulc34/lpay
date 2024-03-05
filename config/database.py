e1d20cb0f129a6e1bbcd24a1b1315aae65c8e324
jpaulc34 <jpcabilan@gmail.com>
2024-03-05 13:42:17 +0800

    fixed database config - mongoclient call

--- config/database.py
+++ config/database.py
@@ -6,12 +6,13 @@ client = MongoClient(config("db_client"))
 db = client.fms
 
 collections = {
-    config("tithe_collection"): config("tithe_collection"),
-    config("user_collection"): config("user_collection")
+    config("tithe_collection"): db[config("tithe_collection")],
+    config("user_collection"): db[config("user_collection")],
 }
 
 class Database:
 
     def collection(collection: str):
         if collection in collections:
+           print(collections[collection])
            return collections[collection]
